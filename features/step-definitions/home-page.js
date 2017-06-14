057411379c3b7f81935c4785bf2aadd0f22c5319
mgallagher1980 <martin.gallagher@nice.org.uk>
2017-06-14 08:48:38 +0000

    added some tests

--- features/step-definitions/home-page.js
+++ features/step-definitions/home-page.js
@@ -1,16 +1,23 @@
+var assert = require('assert');
 var chai = require("chai");
 chai.should();
 
 module.exports = function() {
   this.Given(/^I am on the homepage$/, function() {
-      browser.url('http://test.nice.org.uk/');
+    browser.url('http://test.nice.org.uk/');
   });
 
   this.When(/^I click on the find guidance button$/, function() {
-    $("a[href='/guidance']").click();
+    browser.element("a[href='/guidance']").click();
   });
 
   this.Then(/^I should see the guidance page$/, function() {
+    //browser.debug();
+    //
+    console.log(browser.getUrl());
+    // var exp_url = browser.getUrl();
+    // console.log(exp_url);
+    // assert.equal('https://test.nice.org.uk/guidance', exp_url);
   });
 };
 
