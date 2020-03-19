2ed10b71687e7d7425b1144be78ded562d092c14
lizaadebowale <liza.adebowale@nice.org.uk>
2020-03-19 14:13:08 +0000

    Update run.sh (#8)

--- run.sh
+++ run.sh
@@ -2,7 +2,13 @@
 
 docker-compose down --remove-orphans && docker-compose rm -vf
 docker-compose build && docker-compose up -d
-docker-compose run tests npm run test:teamcity -- --host hub || exit $?
+docker-compose run -e RUNNING_IN_DOCKER=true tests npm run test -- --host hub
 # Copy any error shots out of the docker container
-docker cp functionaltests_tests_run_1:/tests/errorShots ./errorShots_docker
-docker-compose down
+docker cp functional-tests_tests_1:/tests/errorShots ./docker-output/errorShots
+docker-compose logs --no-color > ./docker-output/logs.txt
+
+# Clean up
+docker volume ls
+docker-compose down -v
+docker volume ls
+#docker volume prune -f
