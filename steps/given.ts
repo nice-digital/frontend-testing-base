f806d9d99336de4a5d49c0c7a76ff786ee1d34cc
Cheguegallagher <martin.gallagher@nice.org.uk>
2021-09-08 10:15:49 +0100

    Comment changed

--- steps/given.ts
+++ steps/given.ts
@@ -2,12 +2,11 @@ import { Given } from "@wdio/cucumber-framework";
 import "@nice-digital/wdio-cucumber-steps/lib/given";
 
 // Uncomment below section to write custom step definitions
-/*import { Given } from "cucumber";
-
-Given(
-    /^I open the page "([^"]*)?"$/,
-    openUrl
-);*/
+// import { Given } from "@wdio/cucumber-framework";
+//Given(
+//    /^I open the page "([^"]*)?"$/,
+//    openUrl
+//);
 
 Given("I go to the NICE homepage", async () => {
 	await browser.navigateTo("https://www.nice.org.uk");
